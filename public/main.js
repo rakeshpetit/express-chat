4c0a2b4b5c5390907e3dc263f4faa0265fae1c8a
Rakesh <rakeshpetit@gmail.com>
2019-09-19 17:35:06 +0100

    create and receive messages from client

--- public/main.js
+++ public/main.js
@@ -2,8 +2,17 @@ const socket = io()
 
 socket.on('connect', () => {
     console.log('Connected to server')
+
+    socket.emit('createMessage', {
+        from: 'Client',
+        text: 'Yo man message from client'
+    })
 })
 
 socket.on('disconnect', () => {
     console.log('Disconnected from server')
+})
+
+socket.on('newMessage', (message) => {
+    console.log('New message from server', message)
 })
--- server/server.js
+++ server/server.js
@@ -14,6 +14,15 @@ app.use(express.static(publicPath))
 io.on('connection', (socket) => {
     console.log('New user connected')
 
+    socket.emit('newMessage', {
+        from: 'Server',
+        text: 'Yo man that is a message from server'
+    })
+
+    socket.on('createMessage', (message) => {
+    console.log('Created message', message)
+})
+
     socket.on('disconnect', () => {
         console.log('User was disconnected')
     })
